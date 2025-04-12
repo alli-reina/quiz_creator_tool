0d82c1325cc3fcc4660dfb80c274101f27e10210
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-04-13 00:12:50 +0800

    Get the text inputs from each empty field

--- quiz_maker_app.py
+++ quiz_maker_app.py
@@ -1,2 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+
+def save_question():
+    question_text = entry_question.get().strip()
+    choice_alpha = entry_alpha.get().strip()
+    choice_bravo = entry_bravo.get().strip()
+    choice_charlie = entry_charlie.get().strip()
+    choice_delta = entry_delta.get().strip()
+    correct_answer = correct_answer_var.get().strip().lower()
+    
