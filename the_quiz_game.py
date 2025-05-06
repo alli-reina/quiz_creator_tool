ec7d840d200cccd3194965865b53a6f500114580
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-05-06 20:03:04 +0800

    Updated: Intro screen and added the start button for the game

--- the_quiz_game.py
+++ the_quiz_game.py
@@ -12,3 +12,10 @@ window = pygame.display.set_mode((window_width, window_height))
 pygame.display.set_caption("QUIZ GAME")
 clock = pygame.time.Clock()
 
+# Load the intro assets
+intro_background_image = pygame.image.load("intro_background.png")
+intro_background_image = pygame.transform.scale(intro_background_image, (window_width, window_height))
+
+start_button_image = pygame.image.load("start_button.png")
+start_button_image = pygame.transform.scale(start_button_image, (200, 80))
+start_button_rectangle = start_button_image.get_rect(topleft=(445,340))
