576017f8cf070fe76286724bc8d3211919d5f571
Jonalyn Antoc <jonalynantocc@gmail.com>
2025-05-06 19:55:39 +0800

    Updated: Window height and Window width

--- the_quiz_game.py
+++ the_quiz_game.py
@@ -5,3 +5,10 @@ pygame.init ()
 
 # Load the font wanted
 minecraft_font = pygame.font.Font("Minecraft.ttf", 26)
+
+# Window Settings
+window_width, window_height = 1080, 600
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("QUIZ GAME")
+clock = pygame.time.Clock()
+
